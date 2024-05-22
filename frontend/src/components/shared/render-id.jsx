8504f3fc16d51f2bd0f00be287c91882a6a63a9e
Jamie Pegg <jamiepegg@zoho.com>
2024-05-22 13:42:15 +0100

    Fix issue where breadcrumb doesn't render ID on reload

--- frontend/src/components/shared/render-id.jsx
+++ frontend/src/components/shared/render-id.jsx
@@ -1,57 +1,84 @@
-"use client"
+"use client";
 
-import { useData } from "@/providers/data-provider"
-import { Skeleton } from "@/components/ui/skeleton"
+import { useData } from "@/providers/data-provider";
+import { Skeleton } from "@/components/ui/skeleton";
 
-export function RenderId({id, type}){
-    const {engagements, loadingEngagements, findings, loadingFindings, vulnerabilities, loadingVulnerabilities, clients, loadingClients} = useData()
+export function RenderId({ id, type }) {
+  const {
+    engagements,
+    loadingEngagements,
+    vulnerabilities,
+    loadingVulnerabilities,
+    clients,
+    loadingClients,
+  } = useData();
 
-    if (type === "engagement") {
-        if (loadingEngagements || !engagements) {
-            return <Skeleton className="h-4 w-[150px]" />;
-        } else {
-            const engagement = engagements.find(engagement => engagement._id === id);
-            if (engagement) {
-                return engagement.engagementIdentifier;
-            } else {
-                return <Skeleton className="h-4 w-[150px]" />;
-            }
+  const findFindingById = (id) => {
+    for (let engagement in engagements) {
+      engagement = engagements[engagement];
+      if (engagement?.findings && engagement.findings.length > 0) {
+        for (let finding in engagement.findings) {
+          finding = engagement.findings[finding];
+          if (finding._id === id) {
+            return finding;
+          }
         }
-    } else if (type === "client") {
-        if (loadingClients || !clients) {
-            return <Skeleton className="h-4 w-[150px]" />;
-        } else {
-            const client = clients.find(client => client._id === id);
-            if (client) {
-                return client.clientIdentifier;
-            } else {
-                return <Skeleton className="h-4 w-[150px]" />;
-            }
-        }
-    } else if (type === "finding") {
-        if (loadingFindings || !findings) {
-            return <Skeleton className="h-4 w-[150px]" />;
-        } else {
-            const finding = findings.find(finding => finding._id === id);
-            if (finding) {
-                return finding.findingIdentifier;
-            } else {
-                return <Skeleton className="h-4 w-[150px]" />;
-            }
-        }
-    } else if (type === "vulnerability") {
-        if (loadingVulnerabilities || !vulnerabilities) {
-            return <Skeleton className="h-4 w-[150px]" />;
-        } else {
-            const vulnerability = vulnerabilities.find(vulnerability => vulnerability._id === id);
-            if (vulnerability) {
-                return vulnerability.vulnerabilityIdentifier;
-            } else {
-                return <Skeleton className="h-4 w-[150px]" />;
-            }
-        }
-    } else {
-        // Handle other types if necessary
-        return <Skeleton className="h-4 w-[150px]" />;
+      }
     }
+    return null;
+  };
+
+  if (type === "engagement") {
+    if (loadingEngagements || !engagements) {
+      return <Skeleton className="h-4 w-[150px]" />;
+    } else {
+      const engagement = engagements.find(
+        (engagement) => engagement._id === id,
+      );
+      if (engagement) {
+        return engagement.engagementIdentifier;
+      } else {
+        return <Skeleton className="h-4 w-[150px]" />;
+      }
+    }
+  } else if (type === "client") {
+    if (loadingClients || !clients) {
+      return <Skeleton className="h-4 w-[150px]" />;
+    } else {
+      const client = clients.find((client) => client._id === id);
+      if (client) {
+        return client.clientIdentifier;
+      } else {
+        return <Skeleton className="h-4 w-[150px]" />;
+      }
+    }
+  } else if (type === "finding") {
+    if (loadingEngagements || !engagements) {
+      return <Skeleton className="h-4 w-[150px]" />;
+    } else {
+      const finding = findFindingById(id);
+
+      if (finding) {
+        return finding.findingIdentifier;
+      } else {
+        return <Skeleton className="h-4 w-[150px]" />;
+      }
+    }
+  } else if (type === "vulnerability") {
+    if (loadingVulnerabilities || !vulnerabilities) {
+      return <Skeleton className="h-4 w-[150px]" />;
+    } else {
+      const vulnerability = vulnerabilities.find(
+        (vulnerability) => vulnerability._id === id,
+      );
+      if (vulnerability) {
+        return vulnerability.vulnerabilityIdentifier;
+      } else {
+        return <Skeleton className="h-4 w-[150px]" />;
+      }
+    }
+  } else {
+    // Handle other types if necessary
+    return <Skeleton className="h-4 w-[150px]" />;
+  }
 }
