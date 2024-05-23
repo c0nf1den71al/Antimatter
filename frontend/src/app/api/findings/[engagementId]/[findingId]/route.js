2b482084f8f058dbed87561469b583a6ba1e397b
Jamie Pegg <jamiepegg@zoho.com>
2024-05-23 23:39:33 +0100

    Add wrapper for finding update api endpoint

--- frontend/src/app/api/findings/[engagementId]/[findingId]/route.js
+++ frontend/src/app/api/findings/[engagementId]/[findingId]/route.js
@@ -1,18 +1,43 @@
-import { authOptions } from "../../../auth/[...nextauth]/route"
-import { getServerSession } from "next-auth"
+import { authOptions } from "../../../auth/[...nextauth]/route";
+import { getServerSession } from "next-auth";
 
 // Delete finding
 export async function DELETE(request, { params }) {
-    const session = await getServerSession(authOptions)
-    const accessToken = session.accessToken
-    const res = await fetch(`http://backend:${process.env.ANTIMATTER_API_PORT}/api/findings/${params.engagementId}/${params.findingId}`, {
-        method: "DELETE",
-        headers: {
-            "Authorization": `Bearer ${accessToken}`,
-            "Content-Type": "application/json"
-        }
-    })
+  const session = await getServerSession(authOptions);
+  const accessToken = session.accessToken;
+  const res = await fetch(
+    `http://backend:${process.env.ANTIMATTER_API_PORT}/api/findings/${params.engagementId}/${params.findingId}`,
+    {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+    },
+  );
 
-    const data = await res.json()
-    return Response.json(data)
+  const data = await res.json();
+  return Response.json(data);
+}
+
+// Update finding
+export async function POST(request, { params }) {
+  const session = await getServerSession(authOptions);
+  const accessToken = session.accessToken;
+
+  const body = await request.json();
+  const res = await fetch(
+    `http://backend:${process.env.ANTIMATTER_API_PORT}/api/findings/${params.engagementId}/${params.findingId}`,
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    },
+  );
+
+  const data = await res.json();
+  return Response.json(data);
 }
